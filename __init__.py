d7223591bb323bff698ca24fc81adfe5d381b103
Mert Cikan <mcikan@uni-bremen.de>
2022-01-06 14:40:52 +0100

    Added skill settings

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ class DrillPressVise(MycroftSkill):
     @intent_file_handler('vise.press.drill.intent')
     def handle_vise_press_drill(self, message):
         self.speak_dialog('vise.press.drill')
+        self.speak("Drill Press Vise")
 
 
 def create_skill():
